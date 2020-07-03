225ddf67784bd6d50c8c607d9f15521e0a323bc6
zhouwenshuo <348533713@qq.com>
2020-07-03 17:02:01 +0800

    commit

---
+++ src/com/lemon/class0703/HomeWork.java
@@ -0,0 +1,31 @@
+package com.lemon.class0703;
+
+/**
+ * @time: 2020/7/3 16:59
+ * @author: Mr.Right
+ * @contact: 348533713@qq.com
+ * @file: HomeWork
+ * @desc: ┏┓　　　┏┓+ +
+ * 　　　┏┛┻━━━┛┻┓ + +
+ * 　　　┃　　　　　　　┃
+ * 　　　┃　　　━　　　┃ ++ + + +
+ * 　　 ████━████ ┃+
+ * 　　　┃　　　　　　　┃ +
+ * 　　　┃　　　┻　　　┃
+ * 　　　┃　　　　　　　┃ + +
+ * 　　　┗━┓　　　┏━┛
+ * 　　　　　┃　　　┃
+ * 　　　　　┃　　　┃ + + + +
+ * 　　　　　┃　　　┃　　　　Codes are far away from bugs with the animal protecting
+ * 　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　┃　　　┃
+ * 　　　　　┃　　　┃　　+
+ * 　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　┗┻┛　┗┻┛+ + + +
+ */
+public class HomeWork {
+}
